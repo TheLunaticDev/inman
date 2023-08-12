ae29f8159950b5859154be7028753a2c7fbbce33
Sushanta Banerjee <sushantabanerjee@zohomail.in>
2023-08-12 10:48:41 +0530

    Report creation feature implimented for other services

--- inman/report.py
+++ inman/report.py
@@ -24,5 +24,28 @@ def normal_service():
         " INNER JOIN location ON location.id = inventory.location"
         " INNER JOIN status ON status.id = inventory.status"
         " ORDER BY service_date DESC"
-        ).fetchall()
+    ).fetchall()
+
     return render_template('report-normal-service.html', data=data)
+
+
+@bp.route('/report-other-service', methods=('GET', 'POST'))
+def other_service():
+    db = get_db()
+    data = db.execute(
+        "SELECT inventory.asset_no,"
+        " asset_type.name AS asset_type_name,"
+        " company.name AS company_name,"
+        " location.name AS location_name,"
+        " status.name AS status_name,"
+        " service_date, remarks"
+        " FROM other_service"
+        " INNER JOIN inventory ON inventory.id = other_service.asset_id"
+        " INNER JOIN asset_type ON asset_type.id = inventory.asset_type"
+        " INNER JOIN company ON company.id = inventory.company"
+        " INNER JOIN location ON location.id = inventory.location"
+        " INNER JOIN status ON status.id = inventory.status"
+        " ORDER BY service_date DESC"
+    ).fetchall()
+
+    return render_template('report-other-service.html', data=data)
